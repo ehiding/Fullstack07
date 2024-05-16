10f88c43089decc800267325c26f381102c0f3d6
Nguyen Giap <74212018+ehiding@users.noreply.github.com>
2024-05-17 03:46:50 +0700

    Update index.js

--- btvn/buoi_23/js/index.js
+++ btvn/buoi_23/js/index.js
@@ -3,14 +3,18 @@ console.log(
   `Bài Tập 1: Cho một số nguyên n, trả về số nguyên tố đối xứng nhỏ nhất lớn hơn hoặc bằng n.`
 );
 function nextPrimeNum(num) {
+  // Kiểm tra i chạy từ n + 1
   for (var i = num + 1; ; i++) {
     var isPrime = true;
+    // Cho d chạy từ 2 với điều kiện căn bậc d nhỏ hơn hoặc bằng i
     for (var d = 2; d * d <= i; d++) {
+      // Nếu i chia hết cho d thì đó không phải là số nguyên --> break
       if (i % d === 0) {
         isPrime = false;
         break;
       }
     }
+    // Ngược lại trả về i là nguyên tố
     if (isPrime) {
       return i;
     }
@@ -32,14 +36,18 @@ console.log(
   `Bài Tập 2: Cho hai mảng đã sắp xếp nums1 và nums2 có kích thước lần lượt là m và n, trả về trung vị của hai mảng đã sắp xếp đó.`
 );
 function Median(arr, n) {
+  // n là kích thước của mảng hợp nhất giữa nums1 và nums2
+  // Trường hợp n là số chẵn, thì trung vị sẽ là trung bình cộng của các phần tử chỉ số ((n)/2) và ((n/2)-1)
   if (n % 2 == 0) {
     var z = n / 2;
     var e = arr[z];
     var q = arr[z - 1];
     var ans = (e + q) / 2;
     return ans;
-  } else {
-    var z = Math.floor(n / 2);
+  }
+  // Trường hợp n là số lẻ, trung vị sẽ là chỉ số độ dài mảng hợp nhất (n/2)
+  else {
+    var z = Math.floor(n / 2); // Làm tròn số, ( Ví dụ n = 5 thì sẽ làm tròn z = 2 ), trung vị sẽ là vị trí phần tử thứ 2 của mảng hợp nhất
     return arr[z];
   }
 }
@@ -57,8 +65,9 @@ console.log(`Kích thước n của mảng num2 là: ` + j);
 
 var n = i + j;
 
-var arr = nums1.concat(nums2);
+var arr = nums1.concat(nums2); // Hợp nhất mảng bằng concat
 
+// Sắp xếp mảng theo thứ tự từ bé đến lớn
 arr.sort(function (a, b) {
   return a - b;
 });
@@ -74,15 +83,19 @@ console.log(
   `Bài Tập 3: Cho một mảng số nguyên chưa được sắp xếp nums. Hãy trả về số nguyên dương nhỏ nhất không có trong nums.`
 );
 function firstMissingPositive(nums) {
-  nums.sort((a, b) => a - b);
-  ans = 1;
+  nums.sort((a, b) => a - b); // Sắp xếp mảng theo thứ tự từ bé đến lớn
+  // Đề bài yêu cầu tìm số nguyên dương nên ta khởi tạo số nguyên dương n chạy từ 1
+  var n = 1;
+  // Cho i chạy từ 0 trong vòng lặp for với điều kiện i < kích thước mảng nums
   for (i = 0; i < nums.length; i++) {
-    if (nums[i] == ans) ans++;
+    // Nếu kích thước i của mảng nums bằng với n thì n sẽ tăng lên 1. Ta lặp lại quá trình đến khi n bằng kích thước mảng nums
+    if (nums[i] == n) n++;
   }
-  return ans;
+  // Sau khi vòng lặp kết thúc thì n chính là số nguyên dương cần tìm, ta trả n về cho hàm
+  return n;
 }
 
-var arr = [3, 4, -1, 1];
+var arr = [3, 4, -1, 6, 1, 5, 2];
 console.log(`Mảng chưa được sắp xếp: ` + arr);
 console.log(
   `Số nguyên dương nhỏ nhất không có trong nums là: ` +
