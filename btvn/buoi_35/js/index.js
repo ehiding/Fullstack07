5edf071ba73f067648f0b3ef0f97c8fbf7ff01f8
Nguyen Giap <74212018+ehiding@users.noreply.github.com>
2024-06-27 14:01:25 +0700

    Add files via upload

---
+++ btvn/buoi_35/js/index.js
@@ -0,0 +1,49 @@
+const boldBtn = document.querySelector("#bold-btn");
+const underlineBtn = document.querySelector("#underline-btn");
+const italicBtn = document.querySelector("#italic-btn");
+const colorBtn = document.querySelector("#color-btn");
+
+boldBtn.addEventListener("click", () => {
+  document.execCommand("bold");
+});
+
+underlineBtn.addEventListener("click", () => {
+  document.execCommand("underline");
+});
+
+italicBtn.addEventListener("click", () => {
+  document.execCommand("italic");
+});
+
+colorBtn.addEventListener("input", () => {
+  document.execCommand("forecolor", false, colorBtn.value);
+});
+
+const newBtn = document.querySelector("#new-btn");
+const textBtn = document.querySelector("#text-btn");
+const pdfBtn = document.querySelector("#pdf-btn");
+const content = document.querySelector("#content");
+const fileName = document.querySelector("#filename-input");
+
+newBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+});
+
+textBtn.addEventListener("click", () => {
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(new Blob([content.innerText]));
+  a.download = fileName.value + ".txt";
+  a.click();
+});
+
+pdfBtn.addEventListener("click", () => {
+  html2pdf().from(content).save(fileName.value);
+});
+
+const countCharacters = document.querySelector(".number-of-characters");
+
+function charNumber() {
+  const text = content.value;
+  const textLength = text.length;
+  content.innerText = `${textLength}`;
+}
